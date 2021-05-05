f2e37d998c8303a13df5dc3843d8d7c7655cc809
AbrarMahmud <62346485+AbrarMahmud@users.noreply.github.com>
2021-05-05 18:09:04 +0600

    source_code

---
+++ Source_Code/source_code.ino
@@ -0,0 +1,106 @@
+//MOTOR Right
+const int IN_3=12;
+const int IN_4=13;
+const int EN_right=11; //speed control
+
+//MOTOR Left
+const int IN_1=8;
+const int IN_2=9;
+const int EN_left=10; //speed control
+
+//Variable
+char command;
+const int speed_ =110;
+
+void setup() {
+ Serial.begin(9600);
+ 
+}
+
+void loop() {
+if(Serial.available()>0) { 
+  break_();
+  command = Serial.read();
+  switch(command)
+  {
+   case 'F':
+    forward();
+    break;
+   case 'L':
+    left();
+    break;
+   case 'R':
+    right();
+    break;
+   case 'B':
+    break_();
+    break;
+
+  }
+ }
+
+}
+
+
+void forward()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  analogWrite(EN_left,speed_);
+  
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  analogWrite(EN_right,speed_);
+  
+}
+void left()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,HIGH);
+  analogWrite(EN_left,speed_);
+  
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  analogWrite(EN_right,speed_);
+}
+void right()
+{
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  analogWrite(EN_left,speed_);
+  
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  analogWrite(EN_right,speed_);
+}
+void break_()
+{
+//  digitalWrite(IN_1,LOW);
+//  digitalWrite(IN_2,HIGH);
+//  analogWrite(EN_left,speed_);
+//  
+//  digitalWrite(IN_3,LOW);
+//  digitalWrite(IN_4,HIGH);
+//  analogWrite(EN_right,speed_);
+//  delay(10);
+
+  digitalWrite(IN_1,HIGH);
+  digitalWrite(IN_2,LOW);
+  analogWrite(EN_left,0);
+  
+  digitalWrite(IN_3,HIGH);
+  digitalWrite(IN_4,LOW);
+  analogWrite(EN_right,0);
+  
+}
+void reverse()
+{
+  digitalWrite(IN_1,LOW);
+  digitalWrite(IN_2,HIGH);
+  analogWrite(EN_left,speed_);
+  
+  digitalWrite(IN_3,LOW);
+  digitalWrite(IN_4,HIGH);
+  analogWrite(EN_right,speed_);
+  
+}
